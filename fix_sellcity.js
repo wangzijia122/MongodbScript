9e526a374c4c846a7a5dc8dc96148c935a48c09c
TeddyWang <zijia.wang@pistonint.com>
2021-10-18 11:46:59 +0800

    Update fix_sellcity.js

--- fix_sellcity.js
+++ fix_sellcity.js
@@ -1,11 +1,15 @@
+db.getCollection("hx2car_output_test").find({"SellCityId.history" : null}).size();
+db.hx2car_output_test.find({OnDate : {$lt : ISODate("2021-01-01T16:00:00.000+0000")}}).size()
+db.loadServerScripts();
+fix_sellcity();
+
 db.system.js.save({
 	_id : "fix_sellcity",
 	value : function () {
-				try {
 				var object = db.hx2car_output_test.find({
 				  "SellCityId.history":null
 				}).limit(5).addOption(DBQuery.Option.noTimeout).batchSize(100);
-				print("已获取指定文档...")
+				print("已获取所有需要修改的文档...")
 				var n = 0;
 				while (object.hasNext()) {
 				  var outputdc = object.next();
@@ -16,6 +20,7 @@ db.system.js.save({
 					orgsell = null;
 				  }
 				  ;
+				  try {
 				  outputdc.SellCityId.current = orgsell;
 				  outputdc.SellCityId.history = [];
 				  outputdc.SellCityId.history.push({
@@ -26,12 +31,13 @@ db.system.js.save({
 				  });
 				  db.hx2car_output_test.insertOne(outputdc);
 				  n = n + 1;
-				  print("已修改"+ n +"个文档...")
-				};
-				object.close();
-				print("修改完成！")
+				  print("OrgCarId : " + outputdc.OrgCarId + " 的文档修改成功！");
 				} catch (e) {
-					print("修改失败...")
+					print("OrgCarId : " + outputdc.OrgCarId + " 的文档修改失败...");
+					continue;
+				} 
 			}
+			object.close();
+			print("修改完成，共有" + n + "个文档被修改！");
 			}
 });
